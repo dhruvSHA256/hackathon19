21a33bb8fc9e5a949d32e1d1e672c0ce113e276b
dhruvSHA256 <dhruvsha256@gmail.com>
2019-12-04 18:35:32 +0530

    Added Rich Notification in playground folder

--- CLIENT/internal review/content.js
+++ CLIENT/internal review/content.js
@@ -1,30 +1,16 @@
-var api_url = "http://127.0.0.1:5000/api/?url="
-
-var currurl = window.location.href;
-//alert(currurl);
-var argu = api_url + currurl
-
+ var api_url = "http://127.0.0.1:5000/api/?url=";
+ var currurl = window.location;
+var argu = api_url + currurl;
 function main(object)
-{
+{	
+	console.log(object);
+		if(object["check1"]==0)
+		msg +="";
+		if()
+		msg +="";
+		if()
+		msg +="";
 		
-		//msg = ""
-		//if(object['check1'] != "https")
-			//msg += "This Site <" + object['domain_name']+ "> is Not Secure !!!\n"
-		//if(object['check4']==1)
-			//msg+= "This could be a phising attempt\n"
-		//if(object['check2']!=1)
-			//msg+= "domain of this site is not in our list of verified domains bE cAREFULL \n"
-		
-		//msg+="rank of this site is "	+ object['check3'] + " out of 10 "
-		//ssalert(msg)
-		alert(object["1"])
-		if(object["1"]==1)
-			// https
-
-		
-		if(object["1"]==1)
-			// http
-
 }
 fetch(argu)
 .then(res => res.json())
--- CLIENT/internal review/tmp1.py
+++ CLIENT/internal review/tmp1.py
@@ -6,8 +6,8 @@ import flask
 from flask import request , jsonify
 from flask_cors import CORS
 
-url1 = "https://www.google.com/"
-verified_doms  = ("com" , "org" , "in" , "co.in" , "au") # examples of reputed top level domains
+
+verified_doms  = ("com" , "org" , "in" , "co.in" , "au" , "us" , "ru" , "jp" , "edu") # examples of reputed top level domains
 
 
 
@@ -18,9 +18,6 @@ CORS(app)
 
 @app.route('/api/',methods=['GET'])
 def api_id():
-    flag = 0
-    flag2 = 1
-    msg = ""
     if 'url' in request.args:
         url2 = request.args['url']
 
@@ -35,14 +32,31 @@ def api_id():
 
     
     if(proto=="https"):
-        flag2 = 2
+        check1 = 1
         
-### check for domain ###
-    if(proto=="https"):
-        flag2 = 1
+
+    if(proto=="http:"):
+        check1 = 0
          
    
-    dict = { '1' : flag2 }
+    ### CHECK FOR TOP LEVEL DOMAIN ###
+    
+    _tld = get_tld(url2)
+    
+    for __tld in verified_doms:
+        if(__tld==_tld):
+            check2 = 1
+        else:
+            check2 = 0
+            
+    ### CHECK IF DOMAIN IS IP ADDRESS
+    for i in url:
+        if(i=<9 and i>=0 or i=='.')
+        check3 = 0
+        else:
+        check3 = 1
+
+    dict = { 'check1' : check1 , 'check2' : check2 , 'check3' : check3  }
     return dict
 app.run()
  
