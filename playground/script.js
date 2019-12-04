d311ae6f34bf0447af9853d181e5e8ad4a328126
dhruvSHA256 <dhruvsha256@gmail.com>
2019-12-05 00:07:48 +0530

    Edited rich notifications

--- playground/script.js
+++ playground/script.js
@@ -1,13 +1,23 @@
- chrome.runtime.onMessage.addListener
+
+// var image =  document.getElementById("imageOne");
+
+            // function changeColor()
+            // {
+                // if (image.getAttribute('src') == "button_on.png")
+                // {
+                    // image.src = "button_off.png";
+                // }
+                // else
+                // {
+                    // image.src = "button_on.png";
+                // }
+				
+            // }
+			
+ chrome.extension.onMessage.addListener
  (
   function(msg, sender, sendResponse)
-  { console.log("executed");
-  
-	console.log("script.js recieved")
-		console.log(msg);
+  { console.log("executed in script.js");
+  console.log(msg);
   }
-										
-);
- 
- 
- 
+ )
