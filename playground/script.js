be2baa5bcf7e9d953c6ed9b2f22b260146ad7e5d
dhruvSHA256 <dhruvsha256@gmail.com>
2019-12-04 18:36:50 +0530

    Added Rich Notification in playground folder

---
+++ playground/script.js
@@ -0,0 +1,13 @@
+ chrome.runtime.onMessage.addListener
+ (
+  function(msg, sender, sendResponse)
+  { console.log("executed");
+  
+	console.log("script.js recieved")
+		console.log(msg);
+  }
+										
+);
+ 
+ 
+ 
