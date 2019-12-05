aaed7e74ff05cdb8db8098002b284b19268b8e4e
dhruvSHA256 <dhruvsha256@gmail.com>
2019-12-05 05:58:37 +0530

    Backend Server Code written in Flask

---
+++ playground1/try.py
@@ -0,0 +1,6 @@
+url = "https://paytm.com/"
+
+url2 = ["https://www.pnbindia.in/", "https://www.hdfcbank.com/", "https://www.paypal.com/us/home", "https://www.icicibank.com/", "https://www.phonepe.com/en/", "https://paytm.com/"]
+
+if url in url2:
+    print(url2)
